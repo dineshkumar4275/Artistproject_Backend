3b6398851706a11735c40a792cb8ddd939863c22
sangeetha parthiban <dineshkumar427585@gmail.com>
2026-07-02 17:24:55 +0530

    latest server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://127.0.0.1:5173',
   'http://127.0.0.1:3000',
+  'https://artistproject-backend.vercel.app/api',
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
